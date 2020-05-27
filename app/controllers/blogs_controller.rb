71b974d61edc015d81568effc72d6380ac35ad55
Sam <sam@Sams-MacBook-Pro.local>
2020-05-27 23:34:04 +0300

    remove the json code in the controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -42,10 +42,8 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-       
       else
         format.html { render :edit }
-       
       end
     end
   end
