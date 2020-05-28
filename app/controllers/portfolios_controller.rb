d36fcf026eb47eaa9b87563ae94d715af8a87564
Samkiroko <kirokodev@gmail.com>
2020-05-28 23:18:51 +0300

    Impliment edit and update features on portfolio item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,9 +11,24 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
     respond_to do |format|
       if @portfolio_items.save
-        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
       else
         format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_items = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_items = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_items.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
   end
