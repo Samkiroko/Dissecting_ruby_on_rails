cb6838d45f8afdad0533e75a28d05047dbb694dd
Samkiroko <kirokodev@gmail.com>
2020-05-30 01:35:03 +0300

    Implement image default for portfolio item

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,14 @@ class Portfolio < ApplicationRecord
   def self.angular
     where(subtitle: 'Angular')
   end
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||="https://i.picsum.photos/id/875/600/400.jpg"
+    self.thumb_image ||="https://i.picsum.photos/id/875/350/200.jpg"
+  end
+
 end
+
+
