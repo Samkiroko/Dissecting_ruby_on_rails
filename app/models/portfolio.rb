9cfc6459f5527c6f57dc7ad3e26446df50c3e6ae
Samkiroko <kirokodev@gmail.com>
2020-05-30 02:00:05 +0300

    Implement concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -8,10 +9,9 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||="https://i.picsum.photos/id/875/600/400.jpg"
-    self.thumb_image ||="https://i.picsum.photos/id/875/350/200.jpg"
+    self.main_image ||=Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||=Placeholder.image_generator(height: '350', width: '200')
   end
-
 end
 
 
