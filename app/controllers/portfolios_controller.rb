89bf1adcd1d6dab13d9fdeabd4dff0aa6a7acd5c
Samkiroko <kirokodev@gmail.com>
2020-05-30 13:11:29 +0300

    Implimented form configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,19 +9,21 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_items = Portfolio.new
+    3.times { @portfolio_items.technologies.build }
   end
 
   def create
-    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+
     respond_to do |format|
       if @portfolio_items.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
     end
   end
-
+  
   def edit
     @portfolio_items = Portfolio.find(params[:id])
   end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,8 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
-  accepts_nested_attributes_for :technologies, 
-    reject_if: lambda {|attrs| attrs ['name'].blank?}
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
+
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
@@ -9,11 +10,13 @@ class Portfolio < ApplicationRecord
     where(subtitle: 'Angular')
   end
 
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||=Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||=Placeholder.image_generator(height: '350', width: '200')
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
 
