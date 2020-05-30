267ec01d61442143a00be71843340e443dd88413
Samkiroko <kirokodev@gmail.com>
2020-05-30 21:09:44 +0300

    Implimented basic devise Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
