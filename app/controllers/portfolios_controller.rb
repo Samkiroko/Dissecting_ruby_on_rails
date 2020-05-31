fb26027fa77f38bb5f9ed298f05f7467079d9350
Samkiroko <kirokodev@gmail.com>
2020-06-01 02:33:41 +0300

    Refactoring srong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,7 +13,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_items = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_items.save
@@ -31,7 +31,7 @@ class PortfoliosController < ApplicationController
   def update
     @portfolio_items = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_items.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_items.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
@@ -53,5 +53,13 @@ class PortfoliosController < ApplicationController
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
       format.json { head :no_content }
     end
+  end
+  private
+   def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
   end
 end
