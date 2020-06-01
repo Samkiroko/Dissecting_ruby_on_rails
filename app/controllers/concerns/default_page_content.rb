4e38a65babc3fcde0a44561e75707be02ec9ad19
Samkiroko <kirokodev@gmail.com>
2020-06-01 23:02:52 +0300

    Added nav bar & default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,14 @@
+
+module DefaultPageContent
+  extend ActiveSupport::Concern
+
+  included do
+     before_action :set_page_defaults
+  end
+
+   def set_page_defaults
+    @page_title = "Samuel Kiroko N | Portfolio website"
+    @seo_keywords = "Samuel kiroko portfolio, fullstack ruby, rails,
+                     Javascript, react, html & css"
+   end
+end
