b072ca04b3d166d150e953b22087b5440fd287bc
Samkiroko <kirokodev@gmail.com>
2020-06-03 00:24:51 +0300

    Integrated new gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,5 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
-before_action :set_copyright
-
-  def set_copyright
-    @copyright = SamkirokoViewTool::Renderer.copyright 'Samuel kiroko N', 'All rights reserved'
-  end
 end
 
