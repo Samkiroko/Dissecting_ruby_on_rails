1b1b4f4cf7cbe888bc563825aa348beb63824bc3
Samkiroko <kirokodev@gmail.com>
2020-06-05 01:44:51 +0300

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,62 @@
 module ApplicationHelper
-def login_helper style = ''
-   if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) + 
+  def login_helper style = ''
+    if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path, class: style) +
       " ".html_safe +
-       (link_to "Login", new_user_session_path, class: style)
-   else 
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style 
-   end 
-end
+      (link_to "Login", new_user_session_path, class: style)
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    end
+  end
 
   def source_helper(layout_name)
-     if session[:source] 
-      greeting = "Thanks for visiting me from #{session[:source]}
-       and you are on the #{layout_name} layout"
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
-     end 
+    end
   end
 
   def copyright_generator
-     @copyright = SamkirokoViewTool::Renderer.copyright 'Samuel kiroko N', 'All rights reserved'
+    SamkirokoViewTool::Renderer.copyright 'Samuel kiroko', 'All rights reserved'
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
 end
