82bb67803c2b7ef422147c5244fb82ceaf60ffcb
Samkiroko <kirokodev@gmail.com>
2020-06-05 23:58:08 +0300

    Implemented initial file uploading system via carrierwave for portfolio items

--- Gemfile
+++ Gemfile
@@ -61,4 +61,8 @@ gem 'petergate', '~> 2.0', '>= 2.0.1'
 gem 'font-awesome-sass', '~> 5.9.0'
 gem 'kaminari', '~> 1.2', '>= 1.2.1'
 gem 'jquery-ui-rails', '~> 6.0', '>= 6.0.1'
+gem 'carrierwave', '~> 2.1'
+gem 'mini_magick', '~> 4.10', '>= 4.10.1'
+gem 'carrierwave-aws', '~> 1.5'
+gem 'dotenv-rails', '~> 2.7', '>= 2.7.5'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
---
+++ app/uploaders/portfolio_uploader.rb
@@ -0,0 +1,20 @@
+class PortfolioUploader < CarrierWave::Uploader::Base
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+  
+  storage :file
+ 
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+ 
+end
