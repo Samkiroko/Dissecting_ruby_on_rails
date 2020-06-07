f41f7e6007d5625ca63f6552268af51ea4f98370
Samkiroko <kirokodev@gmail.com>
2020-06-08 00:27:15 +0300

    Created a comment broadcast, configured the websocket route, and updated the comment model with a callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :blog
 
   validates  :Context, presence: true, length: {minimum: 10, maximimum: 1000}
+  after_create_commit {CommentBroadcastJob.perform_later(self)}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,6 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
-  
+  mount ActionCable.server => '/cable'
   root to: 'pages#home'
 end
