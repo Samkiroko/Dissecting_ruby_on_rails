92c00cf45052d82bb96b5592616fda880904a913
Samkiroko <kirokodev@gmail.com>
2020-06-07 17:21:11 +0300

    Refoactored alerts to helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,17 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+  if alert
+      alert_generator alert
+  end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Samuel kiroko N Portfolio", sticky: false)
+  end
+
 end
+
