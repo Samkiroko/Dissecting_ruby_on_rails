1f2a8f880488c95f816329cadb91185be7f01e99
Samkiroko <kirokodev@gmail.com>
2020-06-08 22:18:37 +0300

    built out topic views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :topics, only: [:index, :show]
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout',sign_up: 'register'}
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
