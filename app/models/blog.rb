88ef740adae738fc7521d99f577262c6b4c3f94f
Samkiroko <kirokodev@gmail.com>
2020-06-08 21:13:06 +0300

    Implemented custom scope for recent blogs and hide draft post from non admin

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -7,4 +7,16 @@ class Blog < ApplicationRecord
 
  belongs_to :topic, optional: true
  has_many :comments, dependent: :destroy
+
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
+  end
+
+  def self.recent
+    order("created_at DESC")
+  end
 end
