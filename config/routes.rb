46e8e7de283024352b3f560ad57d8f44b69a78ed
Samkiroko <kirokodev@gmail.com>
2020-06-08 16:48:06 +0300

    Completed comment feature with action cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout',sign_up: 'register'}
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
