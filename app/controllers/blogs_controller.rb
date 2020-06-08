35c072f602044f3eac644e003adaf6ab943d84b7
Samkiroko <kirokodev@gmail.com>
2020-06-08 23:03:25 +0300

    Implemented the blog widget for topics

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,23 +1,25 @@
+
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:update, :create, :destroy, :toggle_status]
   layout "blog"
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
   def index
-  if logged_in?(:site_admin)
-    @blogs = Blog.recent.page(params[:page]).per(5)
-  else
-    @blogs = Blog.published.page(params[:page]).per(5)
-  end
+    if logged_in?(:site_admin)
+      @blogs = Blog.recent.page(params[:page]).per(5)
+    else
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
+    end
     @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    if logged_in?(:site_admin) ||@blog.published?
+    if logged_in?(:site_admin) || @blog.published?
       @blog = Blog.includes(:comments).friendly.find(params[:id])
       @comment = Comment.new
 
@@ -92,5 +94,9 @@ class BlogsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
       params.require(:blog).permit(:title, :body, :topic_id)
+    end
+
+    def set_sidebar_topics
+      @side_bar_topics = Topic.with_blogs
     end
 end
