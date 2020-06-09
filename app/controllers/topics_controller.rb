f11c33570d8e71e2aebc5ac5c05ca1a3701e837c
Samkiroko <kirokodev@gmail.com>
2020-06-09 11:17:21 +0300

    Added new nav bar for the blog page

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,5 @@
 class TopicsController < ApplicationController
-before_action :set_sidebar_topics
+  before_action :set_sidebar_topics
   layout 'blog'
 
   def index
@@ -16,8 +16,9 @@ before_action :set_sidebar_topics
     end
   end
 
+  private
+
   def set_sidebar_topics
     @side_bar_topics = Topic.with_blogs
   end
-
 end
